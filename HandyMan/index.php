7918054c3c2d8a6a85d24b640ace96d4ce295fd6
Jatmiko Budi Baskoro <Jatmiko Budi Baskoro>
2016-03-26 17:58:26 +0700

    Login done!

--- HandyMan/index.php
+++ HandyMan/index.php
@@ -31,13 +31,17 @@ $app = new \Slim\Slim();
  */
 
 // GET route
-$app->get('/');
+$app->get('/', function () {
+	echo "This is GET";
+});
 
 // POST route .method name
-$app->post('/post');
+$app->post('/login', 'login');
 
 // PUT route
-$app->put('/put');
+$app->put('/put',function() {
+	echo "This is PUT";
+});
 
 // PATCH route
 $app->patch('/patch', function () {
@@ -51,35 +55,102 @@ $app->delete(
         echo 'This is a DELETE route';
     }
 );
+
 function login(){
 	$app = \Slim\Slim::getInstance();
-	$app->response()->header("Content-Type","application/json");
+	//$app->response()->header("Content-Type","application/json");
 	$status = false;
-	$json_data = $app->request()->getBody();
-	$data = json_encode($json_data);
-	$username = $data->username;
-	$password = md5($data->password);
-	
+	// $json_data = $app->request()->getBody();
+	// $data = json_encode($json_data);
+	// $username = $data->username;
+	// //$password = md5($data->password);
+	// $password = $data->password;
+
+	$username = $app->request->post('username');
+	$password = $app->request->post('password');
+
 	try{
 		$db = connectDB();
-		$sql = "select * from pengguna where password='$password' and username='$username'";
+		$sql = "select * from user where password='$password' and username='$username'";
 		$result = $db->query($sql);
 		$fetch = $result->fetch(PDO::FETCH_OBJ);
+		//var_dump($fetch);
 		if(empty($fetch)){
 			echo json_encode(to_json(false, "Wrong Username or Password"));
 		}else{
-			$sql_address = "select * from alamat where username='$username'";
+			$user_id = $fetch->id;
+			$sql_address = "select * from user_address where user_id='$user_id'";
 			$result_address = $db->query($sql_address);
-			$fetch_address = $result->fetchAll(PDO::FETCH_OBJ);
+			$fetch_address = $result_address->fetch(PDO::FETCH_OBJ);
+			//var_dump($fetch_address);
 			$res = array(
 					'status' => true,
-					'username' => $fetch['username'],
-					'address' => $fetch_address['alamat']
+					'alamat' => $fetch_address->alamat,
+					'user_id' => $user_id,
+					'username' => $fetch->username,
+					'password' => $fetch->password,
+					'nama'=> $fetch->nama
 			);
-			echo $res;
+			echo json_encode($res);
 		}
 	}catch (PDOException $databaseERROR){
 		echo "Something went wrong";
+	}
+	
+}
+
+function register(){
+	$app = \Slim\Slim::getInstance();
+	$app->response()->header("Content-Type","application/json");
+	//$status = false;
+	$json_data = $app->request()->getBody();
+	$data = json_decode($json_data);
+	$username = $data->username;
+	$password = md5($data->password);
+	$address = $data->address;
+	$name = $data->name;
+	$link_gmaps = $data->link_gmaps;
+	// $password = $data->password;
+
+	//$username = $app->request->post('username');
+	//$password = $app->request->post('password');
+
+	try{
+		$db = connectDB();
+		$sql_check = "select * from user where username='$username'";
+		$check = $db->exec($sql_check);
+		if(empty($check)){
+			$sql = "insert into user (username, password, nama) values ('$username','$password',$name) ";
+			$result = $db->exec($sql);
+			if(!empty($result)){
+				$sql_address = "insert into user_address (user_id, alamat,link_gmaps) values ('$user_id','$alamat','$link_gmaps')";
+				$result = $db->exec("$sql_address");
+			}
+		}
+		else{
+			
+		}
+		
+		
+		//$fetch = $result->fetch(PDO::FETCH_OBJ);
+		//var_dump($fetch);
+		//if(empty($fetch)){
+		//	echo json_encode(to_json(false, "Wrong Username or Password"));
+		echo "Thank you for your register";
+		// }else{
+		// 	$sql_address = "select * from alamat where usernameFK='$username'";
+		// 	$result_address = $db->query($sql_address);
+		// 	$fetch_address = $result_address->fetch(PDO::FETCH_OBJ);
+		// 	//var_dump($fetch_address);
+		// 	$res = array(
+		// 			'status' => true,
+		// 			'username' => $fetch_address->usernameFK,
+		// 			'address' => $fetch_address->alamat
+		// 	);
+		// 	echo json_encode($res);
+		// }
+	}catch (PDOException $databaseERROR){
+		echo "Something went wrong" . $databaseERROR->getMessage();
 	}
 	
 }
@@ -89,15 +160,17 @@ function to_json($status,$message){
 }
 function connectDB(){
 	try{
-		$dbname = 'root';
+		// silahkan ganti dbname,password ke database yang benar
+		$dsn = 'mysql:dbname=handyman;host=127.0.0.1';
+		$dbuser = 'root';
 		$password = '';	
-		$conn = new PDO('mysql:host=localhost; dbname=dbname',$dbname,$password);
+		$conn = new PDO($dsn,$dbuser,$password);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		return $conn;
 	}
 	catch(PDOException $asd){
-		echo 'Error';
+		echo 'Error 123';
 	}
-	return $conn;
 }
 /**
  * Step 4: Run the Slim application
