e102405716e3187d66eb7d64e80774d239576055
Jatmiko Budi Baskoro <Jatmiko Budi Baskoro>
2016-03-27 16:16:15 +0700

    Logout is done!

--- HandyManApp/app/src/main/java/ppl/handyman/MainActivity.java
+++ HandyManApp/app/src/main/java/ppl/handyman/MainActivity.java
@@ -1,9 +1,12 @@
 package ppl.handyman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button logout = (Button) findViewById(R.id.logout);
+        if (logout != null) {
+            logout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent out = new Intent(MainActivity.this,Login.class);
+                    startActivity(out);
+                    finish();
+                }
+            });
+        }
     }
 
     @Override
